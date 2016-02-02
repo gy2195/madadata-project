6098a2489d2c13c604bb140e102f75ee08fbbdc8
gy2195 <yang.jerry12@gmail.com>
2016-02-02 00:14:56 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	entry:"./entry.js";
+	entry: "./entry.js";
 	output: {
 		path: __dirname,
 		filename: "bundle.js"
 	},
 	module: {
 		loaders: [
-			{test: /\.css$/, loader: "style!css"}
+			{ test: /\.css$/, loader: "style!css"}
 		]
 	}
 };
